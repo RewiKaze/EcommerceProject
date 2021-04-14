4f415f67e46ea42625c92e1900a55217419bdb2a
Wasitpol Jongmanasjaroean <41641716+RewiKaze@users.noreply.github.com>
2021-04-14 16:34:55 +0700

    Navbar update

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+// import App from './App';
+import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
+import Shops from "./pages/Shops";
+import Promotion from "./pages/Promotion";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <NavBar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/Shop">
+          <Shops />
+        </Route>
+        <Route path="/Promotion">
+          <Promotion />
+        </Route>
+      </Switch>
+    </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,7 @@
+import "../css/Home.css";
+
+const Home = () => {
+  return <div className="firstSection"></div>;
+};
+
+export default Home;

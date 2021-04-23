cc7df11dcfd05600aaf144054439cba2e32c8fac
pamnana <nupam2543@gmail.com>
2021-04-24 03:11:01 +0700

    Revert "merge home -> main"
    This reverts commit e2dc44644cf3a81e43c8d88d45f79424a50630b0, reversing
    changes made to 994035258c86e5b97b8b5163dcc5bc60e7bf4063.

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,122 +1,17 @@
 import "../css/Home.css";
-import Carousel from 'react-material-ui-carousel';
-import { Paper, Typography, Container, Button } from '@material-ui/core'
-import first from "../image/home/1.png"
-import second from "../image/home/2.png"
-import third from "../image/home/3.png"
-import fourth from "../image/home/4.png"
-import heart from "../image/home/heart.png"
-import fifth from "../image/home/5.png"
-import sixth from "../image/home/6.png"
-import sevth from "../image/home/7.png"
+import Typography from "@material-ui/core/Typography";
 const Home = () => {
-  var items = [
-    {
-      image: first
-    },
-    {
-      image: second
-    },
-    {
-      image: third
-    }
-  ]
-
   return (
     <>
-      {/* first */}
       <div className="firstSection">
         <Typography variant="h1">
-          <span className="head-topic">Shopping,</span><br></br>
-          <span className="head-topic mid">Shipping,</span><br></br>
-          <span className="head-topic">Sleeping</span>
-          <p className="head-content">The best shopping online of furniture, create the world by your hand</p>
+          Shopping,<br></br>Shipping,<br></br>Sleeping
         </Typography>
-        <div>
-          <Carousel maxWidth="sm">
-            {
-              items.map((item, i) => <Item key={i} item={item} />)
-            }
-          </Carousel>
-        </div>
+        <div>Carousel</div>
       </div>
-      {/* second */}
-      <div className="secondSection">
-        <Container>
-          <div className="second-head">
-            <span className="text-topic">Best Seller Product</span><br></br>
-            <span className="text-title">The best of the year</span>
-          </div>
-          <div className="second-content">
-            <div className="best-sell">
-              <img className="best-sell-img" src={fourth} />
-              <div className="best-sell-content">
-                <span className="best-sell-name">Sofa</span>
-                <img className="heart" src={heart} />
-              </div>
-              <span className="best-sell-price">$ 300.00</span>
-            </div>
-            <div className="best-sell">
-              <img className="best-sell-img" src={fourth} />
-              <div className="best-sell-content">
-                <span className="best-sell-name">Sofa</span>
-                <img className="heart" src={heart} />
-              </div>
-              <span className="best-sell-price">$ 300.00</span>
-            </div>
-            <div className="best-sell">
-              <img className="best-sell-img" src={fourth} />
-              <div className="best-sell-content">
-                <span className="best-sell-name">Sofa</span>
-                <img className="heart" src={heart} />
-              </div>
-              <span className="best-sell-price">$ 300.00</span>
-            </div>
-            <div className="best-sell">
-              <img className="best-sell-img" src={fourth} />
-              <div className="best-sell-content">
-                <span className="best-sell-name">Sofa</span>
-                <img className="heart" src={heart} />
-              </div>
-              <span className="best-sell-price">$ 300.00</span>
-            </div>
-          </div>
-        </Container>
-      </div>
-      {/* third */}
-      <div className="thirdSection">
-        <div className="third-topic">
-          <div>
-            <span className="text-topic">Good Furniture,</span>
-            <span className="text-topic bg3"> Good Mood</span>
-          </div>
-          <div>
-            <span className="text-title">Easy choose - Easy choice - Easy Life</span>
-          </div>
-        </div>
-        <div className="third-content">
-          <div className="third-content-left">
-            <img src={fifth} className="third-img left"/>
-          </div>
-          <div className="third-content-right">
-            <img src={sixth} className="third-img right"/>
-            <img src={sevth} className="third-img right"/>
-          </div>
-        </div>
-        <div className="third-foot">
-          <Button className="button1 third-but">See All Catagories</Button>
-        </div>
-      </div>
+      <div className="secondSection"></div>
     </>
   );
 };
-
-function Item(props) {
-  return (
-    <Paper className="carousel-head">
-      <img className="image-head" src={props.item.image} />
-    </Paper>
-  )
-}
 
 export default Home;

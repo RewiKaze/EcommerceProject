2d42d72d6045cb15f1826852086859172b9ed3f2
RewiKaze <lnwkazeazelass@hotmail.co.th>
2021-04-26 13:36:40 +0700

    ีupdate cart

--- frontend/src/components/CardItem.js
+++ frontend/src/components/CardItem.js
@@ -1,83 +1,124 @@
-import React from 'react';
-import {Card, CardActionArea, CardActions, CardContent, CardMedia, Button, Typography, Grid} from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import React from "react";
+import {
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+  Grid,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { useSession } from "../contexts/SessionContext";
+import { useHistory } from "react-router-dom";
 
 // Query Data
 import { PRODUCT_QUERY } from "../graphql/productQuery";
-import { useQuery } from '@apollo/client'
-import {Link} from "react-router-dom";
+import { useQuery } from "@apollo/client";
+import { Link } from "react-router-dom";
 const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1,
-    },
-    paper: {
-        padding: theme.spacing(2),
-        textAlign: 'center',
-        color: theme.palette.text.secondary,
-    },
-    root2: {
-        maxWidth: 345,
-        borderRadius:0
-    },
-    media: {
-        height: 140,
-
-    },
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  },
+  root2: {
+    maxWidth: 345,
+    borderRadius: 0,
+  },
+  media: {
+    height: 140,
+  },
 }));
 
 const CardItem = () => {
-    const classes = useStyles();
-    const { loading, error, data } = useQuery(PRODUCT_QUERY)
-    if (loading) {
-        return 'Loading ...'
-    }
-    if (error) {
-        return 'Error !!'
-    }
-  return (
-      data?.products?.map((product) => (
-          <Grid item xs={3}>
-              <Card className={classes.root2}>
-                  <CardActionArea>
-                      <CardMedia
-                          className={classes.media}
-                          image={product.imageUrl}
-                          title="Contemplative Reptile"
-                      />
-                      <CardContent>
-                          <Typography gutterBottom variant="h5" component="h3">
-                              {product.name}
-                          </Typography>
-                          <Typography style={{color:'#f29559', fontSize:19}}>
-                              {(parseInt(product.price)).toLocaleString('th-TH', {
-                                  style: 'currency',
-                                  currency: 'THB'
-                              }) ?? ""}
-                          </Typography>
-                          <Typography variant="body2" color="textSecondary" component="p" noWrap={true}>
-                              {product.description}
-                          </Typography>
-                          {/*<Typography variant="body2" color="textSecondary" component="p">*/}
-                          {/*    Tags:{product.tag}*/}
-                          {/*</Typography>*/}
+  const { addProductToCart, userCookie, cart } = useSession();
+  const classes = useStyles();
+  const history = useHistory();
+  const { loading, error, data } = useQuery(PRODUCT_QUERY);
+  if (loading) {
+    return "Loading ...";
+  }
+  if (error) {
+    return "Error !!";
+  }
 
-                      </CardContent>
-                  </CardActionArea>
-                  <CardActions>
-                      <Link to={{
-                          pathname: `/product/${product.slug}`,
-                      }} style={{ textDecoration: "none" }}>
-                      <Button size="small" color="primary">
-                          Detail
-                      </Button>
-                      </Link>
-                      <Button size="small" color="primary" variant="contained">
-                          Add to cart
-                      </Button>
-                  </CardActions>
-              </Card>
-          </Grid>
-      ))
-  )
-}
+  const handleAddCart = (id) => {
+    console.log(id, cart);
+    // if (cart?.find((each) => each.id === id)) {
+    //   const result = {
+    //     id: id,
+    //     amount: cart[cart.indexOf(id)].amount + 1,
+    //   };
+    //   addProductToCart(result);
+    // } else {
+    const result = {
+      id: id,
+      amount: 1,
+    };
+    addProductToCart(result);
+  };
+
+  return data?.products?.map((product) => (
+    <Grid item xs={3}>
+      <Card className={classes.root2}>
+        <CardActionArea>
+          <CardMedia
+            className={classes.media}
+            image={product.imageUrl}
+            title="Contemplative Reptile"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h3">
+              {product.name}
+            </Typography>
+            <Typography style={{ color: "#f29559", fontSize: 19 }}>
+              {parseInt(product.price).toLocaleString("th-TH", {
+                style: "currency",
+                currency: "THB",
+              }) ?? ""}
+            </Typography>
+            <Typography
+              variant="body2"
+              color="textSecondary"
+              component="p"
+              noWrap={true}
+            >
+              {product.description}
+            </Typography>
+            {/*<Typography variant="body2" color="textSecondary" component="p">*/}
+            {/*    Tags:{product.tag}*/}
+            {/*</Typography>*/}
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Link
+            to={{
+              pathname: `/product/${product.slug}`,
+            }}
+            style={{ textDecoration: "none" }}
+          >
+            <Button size="small" color="primary">
+              Detail
+            </Button>
+          </Link>
+          <Button
+            onClick={() => {
+              handleAddCart(product._id);
+            }}
+            size="small"
+            color="primary"
+            variant="contained"
+          >
+            Add to cart
+          </Button>
+        </CardActions>
+      </Card>
+    </Grid>
+  ));
+};
 export default CardItem;
--- frontend/src/pages/Cart.js
+++ frontend/src/pages/Cart.js
@@ -1,9 +1,14 @@
-import {Typography, Button, Link} from '@material-ui/core';
-import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
-import LocalShippingIcon from '@material-ui/icons/LocalShipping';
-import { makeStyles } from '@material-ui/core/styles';
-import * as React from 'react';
-import { DataGrid } from '@material-ui/data-grid';
+import { Typography, Button, Link } from "@material-ui/core";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+import LocalShippingIcon from "@material-ui/icons/LocalShipping";
+import { makeStyles } from "@material-ui/core/styles";
+import { NavLink } from "react-router-dom";
+import * as React from "react";
+import { DataGrid } from "@material-ui/data-grid";
+import { useSession } from "../contexts/SessionContext";
+
+import { PRODUCT_QUERY } from "../graphql/productQuery";
+import { useQuery } from "@apollo/client";
 
 import "../css/Cart.css";
 const useStyles = makeStyles((theme) => ({
@@ -14,75 +19,110 @@ const useStyles = makeStyles((theme) => ({
   IconC: {
     marginTop: 1,
   },
-  
 }));
 
 const Cart = () => {
+  const { cart } = useSession();
+  const { loading, error, data } = useQuery(PRODUCT_QUERY);
   const classes = useStyles();
-  return <div className="CartPage">
-    <div className="HeaderCart">
-  <Typography variant="h3"style={{ color: "#F29559" }}>Shopping Cart<ShoppingCartIcon className="IconC"/></Typography>
-  </div>
-  <div style={{ height: 400, width: '100%' }}>
-      <DataGrid rows={rows} columns={columns} pageSize={5} checkboxSelection />
-    </div>
-    <div className="footerAll">
-    <div className="footerT">
-    <div className="FeeText"><LocalShippingIcon className="IconShipping"/>Fee shipping on orders </div>
-    <div className="feeBox">Standard delivery <span className="Receive">Receive by 13 April - 15 April</span></div>
-    <div className="FeePrice">$0.5-$1.5 </div>
-    </div>
-    <div className="footerR">
-    <div className="FeeText2">Merchandise Subtotal (<span>3</span>items) : </div>
-    <div className="Totalp">$268.00</div>
-    <div className="C-button">
-    <Link to="/Checkout">
-      <Button variant="outlined" className={classes.button}>CHECK OUT</Button></Link>
+  if (loading) {
+    return "Loading ...";
+  }
+  if (error) {
+    return "Error !!";
+  }
+  const rows = cart.map((each) => {
+    const eachData = data?.products.find((o) => o._id === each.id);
+    return {
+      id: eachData.name,
+      price: eachData.price,
+      quantity: each.amount,
+      total: each.amount * eachData.price,
+    };
+  });
+  const columns = [
+    { field: "id", headerName: "All", width: 900 },
+    { field: "price", headerName: "Unit Price", width: 200 },
+    {
+      field: "quantity",
+      headerName: "Quantity",
+      width: 220,
+    },
+    {
+      field: "total",
+      headerName: "Total Price",
+      width: 220,
+    },
+    // {
+    //   field: "DeleteI",
+    //   headerName: "Delete",
+    //   width: 200,
+    // },
+  ];
+
+  return (
+    <div className="CartPage">
+      <div className="HeaderCart">
+        <Typography variant="h3" style={{ color: "#F29559" }}>
+          Shopping Cart
+          <ShoppingCartIcon className="IconC" />
+        </Typography>
       </div>
-</div>
-</div>
-<div className="footerSection"></div>
-{/* <div className="footerR">
+      <div style={{ height: 400, width: "100%" }}>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          pageSize={5}
+          checkboxSelection
+        />
+      </div>
+      <div className="footerAll">
+        {/* <div className="footerT">
+          <div className="FeeText">
+            <LocalShippingIcon className="IconShipping" />
+            Fee shipping on orders{" "}
+          </div>
+          <div className="feeBox">
+            Standard delivery{" "}
+            <span className="Receive">Receive by 13 April - 15 April</span>
+          </div>
+          <div className="FeePrice">$0.5-$1.5 </div>
+        </div> */}
+        {/* <div className="footerR"> */}
+        <div className="FeeText2">
+          Merchandise Subtotal (
+          <span>{cart.reduce((total, obj) => obj.amount + total, 0)}</span>{" "}
+          items) :{" "}
+        </div>
+        <div className="Totalp">
+          {rows.reduce((total, obj) => obj.total + total, 0) + " Baht"}
+        </div>
+        <div className="C-button">
+          <Button
+            component={NavLink}
+            to="/checkout"
+            variant="outlined"
+            className={classes.button}
+          >
+            Check out
+          </Button>
+          {/* <Link to="/checkout">
+            <Button variant="outlined" className={classes.button}>
+              CHECK OUT
+            </Button>
+          </Link> */}
+        </div>
+        {/* </div> */}
+      </div>
+      <div className="footerSection"></div>
+      {/* <div className="footerR">
     <div className="FeeText2">Merchandise Subtotal (<span>3</span>items) : </div>
     <div className="Totalp">$268.00</div>
     <div className="C-button">
       <Button variant="outlined" className={classes.button}>CHECK OUT</Button></div>
 </div> */}
     </div>
+  );
 };
 
-const columns = [
-  { field: 'id', headerName: 'All', width: 600 },
-  { field: 'price', headerName: 'Unit Price', width: 220 },
-  
-  {
-    field: 'quantity',
-    headerName: 'Quantity',
-    width: 220,
-  },
-  {
-    field: 'total',
-    headerName: 'Total Price',
-    width: 220,
-  },
-  {
-    field: 'DeleteI',
-    headerName: 'Delete',
-    width: 200,
-  }
-];
-
-const rows = [
-  { id: 1, price: '$89', quantity: 2, total: '$178', DeleteI:'DELETE' },
-  { id: 2, price: '$89', quantity: 1, total: '$89', DeleteI:'DELETE'  },
-  { id: 3, price: '$89', quantity: 2, total: '$178', DeleteI:'DELETE'  },
-  { id: 4, price: '$89', quantity: 1, total: '$89', DeleteI:'DELETE'  },
-  { id: 5, price: '$89', quantity: 2, total: '$178', DeleteI:'DELETE'  },
-  { id: 6, price: '$89', quantity: 1, total: '$89', DeleteI:'DELETE'  },
-  { id: 7, price: '$89', quantity: 1, total: '$89', DeleteI:'DELETE'  },
-  { id: 8, price: '$89', quantity: 1, total: '$89', DeleteI:'DELETE'  },
-  { id: 9, price: '$89', quantity: 1, total: '$89', DeleteI:'DELETE'  },
-];
-
 export default Cart;
-

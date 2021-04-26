b69a742e4285711009be1ec50322f9557eb73738
pamnana <nupam2543@gmail.com>
2021-04-26 09:26:20 +0700

    up customer no route

---
+++ frontend/src/components/User/User-Component.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Link from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import "./User-Component.css";
+import proflie from "./image/proflie.png";
+import BorderColorRoundedIcon from '@material-ui/icons/BorderColorRounded';
+import Button from '@material-ui/core/Button';
+import PersonOutlineOutlinedIcon from '@material-ui/icons/PersonOutlineOutlined';
+import EventAvailableOutlinedIcon from '@material-ui/icons/EventAvailableOutlined';
+import InsertDriveFileOutlinedIcon from '@material-ui/icons/InsertDriveFileOutlined';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    paper: {
+        padding: theme.spacing(3),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+        margin: '2rem',
+        height: '80vh',
+    },
+
+    formControl: {
+        margin: theme.spacing(1),
+        minWidth: 120,
+    },
+    selectEmpty: {
+        marginTop: theme.spacing(2),
+    },
+}));
+
+const User = () => {
+    const classes = useStyles();
+    return (
+        <div className={classes.root} style={{ display: 'flex' }}>
+            <Grid item xs={3}>
+                <Paper className={classes.paper} style={{ display: 'flex', flexDirection: 'column', backgroundColor: "#E5E5E5", boxShadow: '0 0' }}>
+                    <div style={{ display: 'flex' }}>
+                        <div>
+                            <img src={proflie} style={{ width: "4.5rem" }} />
+                        </div>
+                        <div style={{ display: 'flex', flexDirection: 'column', marginLeft: "1rem", textAlign: "left" }}>
+                            <span className="head-title">POTE</span>
+                            <Link href="#">
+                                <BorderColorRoundedIcon style={{ color: "#9E9E9E", height: '0.7rem' }}></BorderColorRoundedIcon><span style={{ fontSize: "0.7rem" }}>Change your profile</span>
+                            </Link>
+                        </div>
+                    </div>
+                    <Button
+                        className={classes.button}
+                        to="/"
+                        exact
+                    >
+                        <PersonOutlineOutlinedIcon></PersonOutlineOutlinedIcon><span> My Account</span>
+                    </Button>
+                    <Button
+                        className={classes.button}
+                        to="/"
+                        exact
+                    >
+                        <EventAvailableOutlinedIcon></EventAvailableOutlinedIcon><span> My Address</span>
+                    </Button>
+                    <Button
+                        className={classes.button}
+                        to="/"
+                        exact
+                    >
+                        <InsertDriveFileOutlinedIcon></InsertDriveFileOutlinedIcon><span> My Order</span>
+                    </Button>
+                    <Button className="logout">Log out</Button>
+                </Paper>
+            </Grid>
+        </div>
+    );
+};
+export default User;
---
+++ frontend/src/components/User/page/User-OrderID.js
@@ -0,0 +1,111 @@
+import React from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import product1 from './../image/product1.png'
+import product2 from './../image/product2.png'
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import Stepper from '@material-ui/core/Stepper';
+import Step from '@material-ui/core/Step';
+import StepLabel from '@material-ui/core/StepLabel';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    paper: {
+        padding: theme.spacing(3),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+        margin: '2rem',
+        height: '80vh',
+    },
+
+    formControl: {
+        margin: theme.spacing(1),
+        minWidth: 120,
+    },
+    selectEmpty: {
+        marginTop: theme.spacing(2),
+    },
+}));
+
+function getSteps() {
+    return ['Request order', 'Shipping Order', 'Success'];
+}
+
+const OrderID = () => {
+    const steps = getSteps();
+    const classes = useStyles();
+
+    return (
+        <Grid item xs={9}>
+            <Paper className={classes.paper}>
+                <div style={{ display: 'flex' }}>
+                    <Grid item xs={1}>
+                        <ArrowBackIosIcon style={{ color: '#202C39' }}></ArrowBackIosIcon>
+                    </Grid>
+                    <Grid item xs={10}>
+                        <span style={{ fontWeight: '700', color: '#202C39' }}>My Order</span>
+                    </Grid>
+                    <Grid item xs={1}></Grid>
+                </div>
+                <hr style={{ height: '0.005rem', backgroundColor: '#E5E5E5', borderWidth: '0', margin: '1rem' }}></hr>
+                <div style={{ color: '#C4C4C4', textAlign: 'left', marginLeft: '1rem', fontSize: '0.75rem' }}><span>My Order /</span><span> Delivered on 17 Jan 2021</span></div>
+                <Stepper alternativeLabel activeStep="3" >
+                    {steps.map((label) => (
+                        <Step key={label}>
+                            <StepLabel>{label}</StepLabel>
+                        </Step>
+                    ))}
+                </Stepper>
+                <div>
+                    <div>
+                        <div id="order1" style={{ display: 'flex', justifyContent: 'space-between', margin: '1rem' }}>
+                            <div style={{ display: 'flex', marginLeft: '1rem' }}>
+                                <img src={product1} style={{ width: '4rem' }} />
+                                <div style={{ textAlign: 'left', marginLeft: '1rem' }}>
+                                    <span style={{ fontWeight: '600', color: '#202C39' }}>ตู้หนังสือ ขนาด 80 ซม. รุ่น Lybrary สีขาว</span><br></br>
+                                    <span style={{ fontWeight: '600', color: '#202C39' }}>X1</span><br></br>
+                                </div>
+                            </div>
+                            <span style={{ fontWeight: '600', color: '#202C39' }}>$39</span>
+                        </div>
+                        <div id="order1" style={{ display: 'flex', justifyContent: 'space-between', margin: '1rem' }}>
+                            <div style={{ display: 'flex', marginLeft: '1rem' }}>
+                                <img src={product1} style={{ width: '4rem' }} />
+                                <div style={{ textAlign: 'left', marginLeft: '1rem' }}>
+                                    <span style={{ fontWeight: '600', color: '#202C39' }}>ตู้หนังสือ ขนาด 80 ซม. รุ่น Lybrary สีขาว</span><br></br>
+                                    <span style={{ fontWeight: '600', color: '#202C39' }}>X1</span><br></br>
+                                </div>
+                            </div>
+                            <span style={{ fontWeight: '600', color: '#202C39' }}>$39</span>
+                        </div>
+                        <div style={{ display: 'flex', justifyContent: 'space-between', margin: '1rem', marginLeft: '2rem' }}>
+                            <span style={{ fontWeight: '600', color: '#202C39' }}>Order</span>
+                            <span style={{ fontWeight: '600', color: '#202C39' }}>$78</span>
+                        </div>
+                        <div style={{ display: 'flex', justifyContent: 'space-between', margin: '1rem', marginLeft: '2rem' }}>
+                            <span style={{ fontWeight: '600', color: '#202C39' }}>Shipping</span>
+                            <span style={{ fontWeight: '600', color: '#202C39' }}>$0</span>
+                        </div>
+                        <div style={{ display: 'flex', justifyContent: 'space-between', margin: '1rem', marginLeft: '2rem' }}>
+                            <span style={{ fontWeight: '800', color: '#202C39' }}>Total</span>
+                            <span style={{ fontWeight: '800', color: '#202C39' }}>$78</span>
+                        </div>
+                        <div style={{ display: 'flex', marginTop: '2.5rem', borderRadius: '0.5rem' }}>
+                            <Grid item xs={4} style={{ backgroundColor: '#202C39', color: 'white', padding: '1rem' }}>
+                                Payment Method
+                                </Grid>
+                            <Grid item xs={8} style={{ backgroundColor: '#E5E5E5', color: '#F29559', padding: '1rem' }}>
+                                Siam Commercial Bank Public Company Limited (SCB)
+                                </Grid>
+                        </div>
+                    </div>
+                </div>
+            </Paper>
+        </Grid>
+    )
+}
+export default OrderID;

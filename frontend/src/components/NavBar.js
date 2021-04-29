2f10d99b3e103467a9ea5be035c958adf7b87f96
PattharananZ <42967288+PattharananZ@users.noreply.github.com>
2021-04-29 17:56:12 +0700

    Update NavBar.js

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -8,6 +8,10 @@ import logo from "../image/logo.png";
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import { useSession } from "../contexts/SessionContext";
 import Badge from "@material-ui/core/Badge";
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+
 
 const useStyles = makeStyles((theme) => ({
   title: {
@@ -15,6 +19,9 @@ const useStyles = makeStyles((theme) => ({
   },
   styleBar: {
     backgroundColor: "#202c39",
+  },
+  Menus: {
+    display: "flex",
   },
   button: {
     color: "#f29559",
@@ -27,31 +34,42 @@ const useStyles = makeStyles((theme) => ({
 
 }));
 
+
 const NavBar = () => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   const { loading, user, logout: handleLogout, } = useSession();
   const userBox = useMemo(() => {
     if (loading) {
       return (
-          <span>Loading ...</span>
+        <span>Loading ...</span>
       )
     }
     if (user) {
       return (
         <React.Fragment>
           {user?.type === "ADMIN" ? (
-              <Button
-                  style={{color:"#F29559"}}
-                  component={NavLink}
-                  type="button"
-                  activeStyle={{ borderBottom: "5px solid #f29559" }}
-                  to="/admin"
-              >
-                Admin
-              </Button>
-          ) : <Button style={{color:"#F29559"}}
+            <Button
+              style={{ color: "#F29559" }}
               component={NavLink}
+              type="button"
               activeStyle={{ borderBottom: "5px solid #f29559" }}
-              to="/cart"
+              to="/admin"
+            >
+              Admin
+            </Button>
+          ) : <Button style={{ color: "#F29559" }}
+            component={NavLink}
+            activeStyle={{ borderBottom: "5px solid #f29559" }}
+            to="/cart"
           >
             <Badge badgeContent={4} color="error">
               <ShoppingCartIcon />
@@ -59,25 +77,35 @@ const NavBar = () => {
             {/*Cart*/}
           </Button>
           }
-          <Button>
-            <Avatar style={{backgroundColor:"#F2D492"}}>{user?.name[0]}</Avatar>{'  '}
-            <span style={{color:"#F29559", marginLeft:10}}>Hello,K.{user?.name}</span>
+          <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+            <Avatar style={{ backgroundColor: "#F2D492" }}>{user?.name[0]}</Avatar>{'  '}
+            <span style={{ color: "#F29559", marginLeft: 10 }}>Hello,K.{user?.name}<ArrowDropDownIcon /></span>
           </Button>
+          <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose}>Profile</MenuItem>
+        <MenuItem onClick={handleClose}>My account</MenuItem>
+      </Menu>
 
-          <Button style={{color:"#F29559"}}
+          <Button style={{ color: "#F29559" }}
             onClick={handleLogout}
             type="button"
           >
-            <ExitToAppIcon/> Logout
+            <ExitToAppIcon /> Logout
           </Button>
         </React.Fragment>
       );
     }
     return (<Button
-        component={NavLink}
-        style={{color:"#F29559"}}
-        activeStyle={{ borderBottom: "5px solid #f29559" }}
-        to="/login"
+      component={NavLink}
+      style={{ color: "#F29559" }}
+      activeStyle={{ borderBottom: "5px solid #f29559" }}
+      to="/login"
     >
       <PersonIcon /> Login
     </Button>)
@@ -91,56 +119,56 @@ const NavBar = () => {
   //     path: "/admin",
   //   }) == null
   // ) {
-    return (
-      <>
-        <AppBar position="fixed" className={classes.styleBar}>
-          <Toolbar>
-            <div>
-              <Button
-                className={classes.button}
-                component={NavLink}
-                to="/"
-                exact
-              >
-                <img src={logo} width="50vw" />
+  return (
+    <>
+      <AppBar position="fixed" className={classes.styleBar}>
+        <Toolbar>
+          <div>
+            <Button
+              className={classes.button}
+              component={NavLink}
+              to="/"
+              exact
+            >
+              <img src={logo} width="50vw" />
+            </Button>
+            <Button
+              className={classes.button}
+              component={NavLink}
+              activeStyle={{ borderBottom: "5px solid #f29559" }}
+              to="/"
+              exact
+            >
+              Home
               </Button>
-              <Button
-                className={classes.button}
-                component={NavLink}
-                activeStyle={{ borderBottom: "5px solid #f29559" }}
-                to="/"
-                exact
-              >
-                Home
+            <Button
+              className={classes.button}
+              component={NavLink}
+              activeStyle={{ borderBottom: "5px solid #f29559" }}
+              to="/product"
+            >
+              Product
               </Button>
-              <Button
-                className={classes.button}
-                component={NavLink}
-                activeStyle={{ borderBottom: "5px solid #f29559" }}
-                to="/product"
-              >
-                Product
+            <Button
+              className={classes.button}
+              component={NavLink}
+              activeStyle={{ borderBottom: "5px solid #f29559" }}
+              to="/promotion"
+            >
+              Promotion
               </Button>
-              <Button
-                className={classes.button}
-                component={NavLink}
-                activeStyle={{ borderBottom: "5px solid #f29559" }}
-                to="/promotion"
-              >
-                Promotion
-              </Button>
-            </div>
+          </div>
 
-            <div className={classes.navLinkRight}>
+          <div className={classes.navLinkRight}>
 
-              {userBox}
+            {userBox}
 
-            </div>
-          </Toolbar>
-        </AppBar>
-        <Toolbar />
-      </>
-    );
+          </div>
+        </Toolbar>
+      </AppBar>
+      <Toolbar />
+    </>
+  );
   // }
   // else {
   //   return null;

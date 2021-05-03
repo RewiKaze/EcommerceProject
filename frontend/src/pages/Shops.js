c79de6bcb2c602849d05f52dd768b26fc87b6798
pamnana <nupam2543@gmail.com>
2021-05-03 08:53:25 +0700

    up button

--- frontend/src/pages/Shops.js
+++ frontend/src/pages/Shops.js
@@ -69,7 +69,7 @@
 // export default Shops;
 import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { Typography } from '@material-ui/core'
+import { Typography, Button } from '@material-ui/core'
 import Carousel from 'react-material-ui-carousel';
 import first from './../image/product/1.png';
 import third from './../image/product/3.png';
@@ -78,6 +78,10 @@ import h1 from './../image/product/h1.png';
 import h2 from './../image/product/h2.png';
 import h3 from './../image/product/h3.png';
 import h4 from './../image/product/h4.png';
+import SingleBedIcon from '@material-ui/icons/SingleBed';
+import BathtubIcon from '@material-ui/icons/Bathtub';
+import KitchenIcon from '@material-ui/icons/Kitchen';
+import WeekendIcon from '@material-ui/icons/Weekend';
 import "./../css/Shop.css";
 import {
   Grid,
@@ -92,6 +96,7 @@ import PageSelect from "../components/PageSelect";
 // query Item
 import { PRODUCT_QUERY } from "../graphql/productQuery";
 import { useQuery } from "@apollo/client";
+
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
@@ -119,6 +124,7 @@ const Shops = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [productsPerPage] = useState(8);
   const { loading, error, data } = useQuery(PRODUCT_QUERY);
+  //const [type, setType] = useState('');
   if (loading) {
     return "Loading ...";
   }
@@ -128,112 +134,136 @@ const Shops = () => {
   const indexOfLastProducts = currentPage * productsPerPage;
   const indexOfFirstProducts = indexOfLastProducts - productsPerPage;
   const currentProducts = data.products.slice(
-      indexOfFirstProducts,
-      indexOfLastProducts
+    indexOfFirstProducts,
+    indexOfLastProducts
   );
   const paginate = (pageNumber) => {
     setCurrentPage(pageNumber);
   };
+  // const HandleChange = (type) =>{
+  //   setType(type); 
+  // }
 
   return (
-      <React.Fragment>
-        <div className="firstSection-shop">
-          <Typography variant="h2">
-            <span className="topic-shop">Noble</span><br></br>
-            <span className="topic-shop mid">Experience</span><br></br>
-            <span className="topic-shop">Your Home</span>
-            <p className="content-shop">The best shopping online of furniture, create the world by your hand</p>
-            <div className="set-pic">
-
-            </div>
-          </Typography>
-          <div>
-            <Carousel maxWidth="sm">
-              {
-                items.map((item, i) => <Item key={i} item={item} />)
-              }
-            </Carousel>
+    <React.Fragment>
+      <div className="firstSection-shop">
+        <Typography variant="h2">
+          <span className="topic-shop">Noble</span><br></br>
+          <span className="topic-shop mid">Experience</span><br></br>
+          <span className="topic-shop">Your Home</span>
+          <p className="content-shop">The best shopping online of furniture, create the world by your hand</p>
+          <div className="set-pic">
+            <img src={h1} />
+            <img src={h2} />
+            <img src={h3} />
+            <img src={h4} />
           </div>
+        </Typography>
+        <div>
+          <Carousel maxWidth="sm">
+            {
+              items.map((item, i) => <Item key={i} item={item} />)
+            }
+          </Carousel>
         </div>
-        <hr />
-        <div className={classes.root}>
-          <Grid container spacing={3} style={{ marginTop: 10 }}>
-            {/*<Grid item xs={12}>*/}
-            {/*  <Grid container spacing={3}>*/}
-            {/*    <Grid item xs={8}>*/}
-            {/*      <TextField*/}
-            {/*        label="Search"*/}
-            {/*        variant="outlined"*/}
-            {/*        style={{ width: "100%" }}*/}
-            {/*      />*/}
-            {/*    </Grid>*/}
-            {/*    <Grid item xs={2}>*/}
-            {/*      <FormControl variant="outlined" style={{ width: "100%" }}>*/}
-            {/*        <InputLabel htmlFor="outlined-age-native-simple">*/}
-            {/*          Category*/}
-            {/*        </InputLabel>*/}
-            {/*        <Select*/}
-            {/*          native*/}
-            {/*          label="Category"*/}
-            {/*          inputProps={{*/}
-            {/*            name: "Category",*/}
-            {/*            id: "outlined-age-native-simple",*/}
-            {/*          }}*/}
-            {/*        >*/}
-            {/*          <option aria-label="None" />*/}
-            {/*        </Select>*/}
-            {/*      </FormControl>*/}
-            {/*    </Grid>*/}
-            {/*    <Grid item xs={2}>*/}
-            {/*      <TextField*/}
-            {/*        label="Tags"*/}
-            {/*        variant="outlined"*/}
-            {/*        style={{ width: "100%" }}*/}
-            {/*      />*/}
-            {/*    </Grid>*/}
-            {/*  </Grid>*/}
-            {/*</Grid>*/}
-            {/*<Grid item xs={3}>*/}
-            {/*  <Paper*/}
-            {/*    className={classes.paper}*/}
-            {/*    style={{ color: "#f29559", fontWeight: "bold" }}*/}
-            {/*  >*/}
-            {/*    CATEGORY*/}
-            {/*    <hr />*/}
-            {/*    <p style={{ fontWeight: "lighter" }}>All</p>*/}
-            {/*    <p style={{ fontWeight: "lighter" }}>Bedroom</p>*/}
-            {/*    <p style={{ fontWeight: "lighter" }}>Bathroom</p>*/}
-            {/*    <p style={{ fontWeight: "lighter" }}>Kitchen</p>*/}
-            {/*    <p style={{ fontWeight: "lighter" }}>Living room</p>*/}
-            {/*    <p style={{ fontWeight: "lighter" }}>Other</p>*/}
-            {/*  </Paper>*/}
-            {/*</Grid>*/}
-            <Grid item xs={12} style={{ paddingInline: 20 }}>
-              <Grid container spacing={3}>
-                {/* {console.log(data.products)} */}
-                {currentProducts.map((each) => {
-                  return <CardItem product={each} />;
-                })}
-              </Grid>
-            </Grid>
-            <Grid container justify="center">
-              <PageSelect
-                  productsPerPage={productsPerPage}
-                  totalProducts={data.products.length}
-                  paginate={paginate}
-              />
+      </div>
+      <hr />
+      <div className="filter-bar">
+        <Button >
+          <SingleBedIcon style={{ fontSize: '2.5rem' }}></SingleBedIcon>
+          <span>Bedroom</span>
+        </Button>
+        <Button >
+          <BathtubIcon style={{ fontSize: '2.5rem' }}></BathtubIcon>
+          <span>Bathroom</span>
+        </Button>
+        <Button >
+          <KitchenIcon style={{ fontSize: '2.5rem' }}></KitchenIcon>
+          <span>Kitchen</span>
+        </Button>
+        <Button >
+          <WeekendIcon style={{ fontSize: '2.5rem' }}></WeekendIcon>
+          <span>Living Room</span>
+        </Button>
+      </div>
+      <div className={classes.root}>
+        <Grid container spacing={3} style={{ marginTop: 10 }}>
+          {/*<Grid item xs={12}>*/}
+          {/*  <Grid container spacing={3}>*/}
+          {/*    <Grid item xs={8}>*/}
+          {/*      <TextField*/}
+          {/*        label="Search"*/}
+          {/*        variant="outlined"*/}
+          {/*        style={{ width: "100%" }}*/}
+          {/*      />*/}
+          {/*    </Grid>*/}
+          {/*    <Grid item xs={2}>*/}
+          {/*      <FormControl variant="outlined" style={{ width: "100%" }}>*/}
+          {/*        <InputLabel htmlFor="outlined-age-native-simple">*/}
+          {/*          Category*/}
+          {/*        </InputLabel>*/}
+          {/*        <Select*/}
+          {/*          native*/}
+          {/*          label="Category"*/}
+          {/*          inputProps={{*/}
+          {/*            name: "Category",*/}
+          {/*            id: "outlined-age-native-simple",*/}
+          {/*          }}*/}
+          {/*        >*/}
+          {/*          <option aria-label="None" />*/}
+          {/*        </Select>*/}
+          {/*      </FormControl>*/}
+          {/*    </Grid>*/}
+          {/*    <Grid item xs={2}>*/}
+          {/*      <TextField*/}
+          {/*        label="Tags"*/}
+          {/*        variant="outlined"*/}
+          {/*        style={{ width: "100%" }}*/}
+          {/*      />*/}
+          {/*    </Grid>*/}
+          {/*  </Grid>*/}
+          {/*</Grid>*/}
+          {/*<Grid item xs={3}>*/}
+          {/*  <Paper*/}
+          {/*    className={classes.paper}*/}
+          {/*    style={{ color: "#f29559", fontWeight: "bold" }}*/}
+          {/*  >*/}
+          {/*    CATEGORY*/}
+          {/*    <hr />*/}
+          {/*    <p style={{ fontWeight: "lighter" }}>All</p>*/}
+          {/*    <p style={{ fontWeight: "lighter" }}>Bedroom</p>*/}
+          {/*    <p style={{ fontWeight: "lighter" }}>Bathroom</p>*/}
+          {/*    <p style={{ fontWeight: "lighter" }}>Kitchen</p>*/}
+          {/*    <p style={{ fontWeight: "lighter" }}>Living room</p>*/}
+          {/*    <p style={{ fontWeight: "lighter" }}>Other</p>*/}
+          {/*  </Paper>*/}
+          {/*</Grid>*/}
+          <Grid item xs={12} style={{ paddingInline: 20 }}>
+            <Grid container spacing={3}>
+              {/* {console.log(data.products)} */}
+              {currentProducts.map((each) => {
+                return <CardItem product={each} />;
+              })}
             </Grid>
           </Grid>
-        </div>
-      </React.Fragment>
+          <Grid container justify="center">
+            <PageSelect
+              productsPerPage={productsPerPage}
+              totalProducts={data.products.length}
+              paginate={paginate}
+            />
+          </Grid>
+        </Grid>
+      </div>
+    </React.Fragment>
   );
 };
 
 function Item(props) {
   return (
-      <Paper className="carousel-head-shop">
-        <img className="image-head-shop" alt={"test"} src={props.item.image} />
-      </Paper>
+    <Paper className="carousel-head-shop">
+      <img className="image-head-shop" alt={"test"} src={props.item.image} />
+    </Paper>
   )
 }
 
